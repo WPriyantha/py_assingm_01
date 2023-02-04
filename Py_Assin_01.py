d5e753d7efef44d5d7985dce8ac9aff152b0c856
WPriyantha <118719261+WPriyantha@users.noreply.github.com>
2023-02-04 20:56:23 +0530

    Add files via upload

---
+++ Py_Assin_01.py
@@ -0,0 +1,10 @@
+import random
+
+fortune = random.randint(1,3)
+
+if fortune == 1:
+    print("Today will be a lucky day")
+elif fortune == 2:
+    print("You will meet someone new and interesting")
+elif fortune == 3:
+    print("Watch out for obstacles in your path.")
